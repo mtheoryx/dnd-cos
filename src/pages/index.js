3f6e941a1380f1033057bf6e4839294077d0c2db
David Poindexter <drpoindexter@gmail.com>
2021-11-30 17:47:41 -0500

    Test branch-based previews

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const App = () => (
-  <h1>Welcome to my CoS Notes with auto build and auto branches</h1>
+  <h1>
+    Welcome to my CoS Notes with auto build and auto branches with name filter
+  </h1>
 );
 
 export default App;
